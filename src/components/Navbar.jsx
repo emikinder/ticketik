0ac786c1191e91658b40ba4be7141982b82c1c5d
Emiliano <emikinderk@gmail.com>
2023-05-21 21:56:53 +0200

    refactor navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,69 +13,83 @@ export const Navbar = () => {
 
     return (
         <nav className="fixed backdrop-blur-lg w-full md:h-auto md:flex md:justify-center">
-            <div className="px-10 py-8 md:flex md:justify-between md:items-center md:w-10/12">
+            {/* desktop */}
+            <div className="max-[640px]:hidden max-[1000px]:w-full sm:hidden md:flex justify-between items-center w-10/12 px-10 py-8">
                 <div className="flex justify-between">
                     <Link to="/" onClick={() => window.scrollTo(0, 0)}>
                         <FontAwesomeIcon
-                            className="w-8 h-8 md:w-12 md:h-12 hover:text-customGreen"
+                            className="w-12 h-12 hover:text-customGreen"
                             icon={faRebel}
                         />
                     </Link>
-                    <div className="flex items-center md:hidden">
+                </div>
+                <NavbarItems styles="flex justify-between w-7/12" />
+                <div className="flex justify-between w-20 mt-0">
+                    <Link to="/">
+                        <FontAwesomeIcon
+                            className="w-7 h-7 hover:text-customGreen"
+                            icon={faInstagram}
+                        />
+                    </Link>
+                    <Link to="/">
+                        <FontAwesomeIcon
+                            className="w-7 h-7 hover:text-customGreen"
+                            icon={faTwitter}
+                        />
+                    </Link>
+                </div>
+            </div>
+
+            {/* mobile */}
+            <div className="px-10 py-8 md:hidden">
+                <div className="flex justify-between">
+                    <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                        <FontAwesomeIcon className="w-8 h-8" icon={faRebel} />
+                    </Link>
+                    <div className="flex items-center">
                         <button onClick={() => setNavBarOpen(!navBarOpen)}>
                             <FontAwesomeIcon
                                 className="w-5 h-5"
-                                icon={navBarOpen ? faBars : faX}
+                                icon={navBarOpen ? faX : faBars}
                             />
                         </button>
                     </div>
                 </div>
-                {navBarOpen && (
-                    <>
-                        <ul className="md:flex md:justify-between md:w-3/6">
-                            <li className="pt-2">
-                                <Link className="navbar__item" to="/">
-                                    Home
-                                </Link>
-                            </li>
-                            <li className="pt-2">
-                                <Link className="navbar__item" to="/results">
-                                    Results
-                                </Link>
-                            </li>
-                            <li className="pt-2">
-                                <Link className="navbar__item" to="/sports">
-                                    Sports
-                                </Link>
-                            </li>
-                            <li className="pt-2">
-                                <Link className="navbar__item" to="/services">
-                                    Services
-                                </Link>
-                            </li>
-                            <li className="pt-2">
-                                <Link className="navbar__item" to="/contact">
-                                    Contact us
-                                </Link>
-                            </li>
-                        </ul>
-                        <div className="mt-10 flex justify-between w-20 md:mt-0">
-                            <Link to="/">
-                                <FontAwesomeIcon
-                                    className="w-7 h-7 hover:text-customGreen"
-                                    icon={faInstagram}
-                                />
-                            </Link>
-                            <Link to="/">
-                                <FontAwesomeIcon
-                                    className="w-7 h-7 hover:text-customGreen"
-                                    icon={faTwitter}
-                                />
-                            </Link>
-                        </div>
-                    </>
-                )}
+                {navBarOpen && <NavbarItems />}
             </div>
         </nav>
+    );
+};
+
+// eslint-disable-next-line react/prop-types
+const NavbarItems = ({ styles }) => {
+    return (
+        <ul className={styles}>
+            <li className="pt-2">
+                <Link className="navbar__item" to="/">
+                    Home
+                </Link>
+            </li>
+            <li className="pt-2">
+                <Link className="navbar__item" to="/results">
+                    Results
+                </Link>
+            </li>
+            <li className="pt-2">
+                <Link className="navbar__item" to="/sports">
+                    Sports
+                </Link>
+            </li>
+            <li className="pt-2">
+                <Link className="navbar__item" to="/services">
+                    Services
+                </Link>
+            </li>
+            <li className="pt-2">
+                <Link className="navbar__item" to="/contact">
+                    Contact us
+                </Link>
+            </li>
+        </ul>
     );
 };
