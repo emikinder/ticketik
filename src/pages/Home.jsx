9258ef4ad30bad38a4717312e7c9a02bde368a9a
Emiliano <emikinderk@gmail.com>
2023-06-12 22:59:35 +0200

    no-ticket/fixs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFutbol } from "@fortawesome/free-solid-svg-icons";
 import { LEAGUES_DATA } from "../assets/data/FootballData";
 import OG from "../assets/images/OG.png";
+import NBA from "../assets/images/nba.png";
+import MUNDIAL from "../assets/images/2026.jpg";
 import Table from "../components/Table";
 import { FootballTable } from "../components/FootballTable";
 
@@ -12,7 +14,7 @@ export const Home = () => {
             <section className="flex justify-center items-center h-[80vh]">
                 <div className="w-full px-10 mt-10 md:flex md:px-0 md:w-10/12">
                     <div className="items-center md:w-8/12">
-                        <h1 className="w-10/12 text-center text-3xl md:w-full md:text-left md:text-4xl lg:text-6xl">
+                        <h1 className="w-full text-center text-3xl md:w-full md:text-left md:text-4xl lg:text-6xl">
                             Take your{" "}
                             <span className="text-customGreen">passion</span>{" "}
                             for <span className="text-customGreen">sports</span>{" "}
@@ -22,11 +24,11 @@ export const Home = () => {
                 </div>
             </section>
 
-            <section className="h-screen">
+            <section className="md:h-screen">
                 <div className="flex justify-center">
                     <div className="md:flex md:w-10/12">
                         <div className="md:w-6/12">
-                            <h2 className="text-2xl">
+                            <h2 className="text-2xl text-center md:text-left">
                                 <FontAwesomeIcon
                                     className="hover:animate-spin"
                                     icon={faFutbol}
@@ -37,7 +39,7 @@ export const Home = () => {
                                 </span>{" "}
                                 Leagues
                             </h2>
-                            <div className="items-center mt-6">
+                            <div className="items-center mt-6 flex justify-center md:block md:justify-normal">
                                 {LEAGUES_DATA.map((league, i) => (
                                     // <Table key={i} league={league} />
                                     <FootballTable key={i} league={league} />
@@ -45,7 +47,7 @@ export const Home = () => {
                             </div>
                         </div>
                         <div className="md:w-6/12">
-                            <div className="w-full px-8 py-4 text-white bg-customGreen rounded">
+                            <div className="w-full px-8 py-4 text-white bg-customGreen md:rounded">
                                 <p className="text-3xl">
                                     All Football Matches Fixtures & Tips for
                                     Tomorrow
@@ -57,16 +59,21 @@ export const Home = () => {
                                     and more to help you decide your next bet.
                                 </p>
                             </div>
-                            <div className="mt-8 w-1/2 flex justify-between items-start">
+                            <div className="mt-8 mb-8 w-1/2 flex justify-between items-start">
                                 <img
-                                    className="rounded-md w-10/12"
-                                    src="https://vbsports.es/wp-content/uploads/2018/08/nba-banner.jpg"
+                                    className="rounded-md h-40"
+                                    src={NBA}
                                     alt="NBA"
                                 />
                                 <img
                                     className="border rounded-md w-10/12"
                                     src={OG}
                                     alt="Olympic Games"
+                                />
+                                <img
+                                    className="border rounded-md w-10/12"
+                                    src={MUNDIAL}
+                                    alt="Mundial"
                                 />
                             </div>
                         </div>
