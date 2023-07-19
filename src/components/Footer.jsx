7bc8a096a7bde09f77e18a9e44ba1b5b8c8988f6
Emiliano <emikinderk@gmail.com>
2023-07-19 20:23:48 +0200

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,77 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => {
     return (
-        <div className="p-10 w-full h-[40vh] bg-customGreen text-white">
-            <div className="flex justify-between w-20 mt-0">
-                <Link to="/">
-                    <FontAwesomeIcon
-                        className="w-7 h-7 hover:text-customGreen"
-                        icon={faInstagram}
-                    />
-                </Link>
-                <Link to="/">
-                    <FontAwesomeIcon
-                        className="w-7 h-7 hover:text-customGreen"
-                        icon={faTwitter}
-                    />
-                </Link>
-            </div>
+        <div className="flex justify-center w-full h-[40vh] bg-customGreen text-white">
+            <section className="flex flex-col md:flex-row md:justify-between w-10/12">
+                <div className="flex mt-5 md:w-4/12">
+                    <div className="w-1/2">
+                        <ul>
+                            <li className="pt-2">
+                                <Link className="" to="/">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="pt-2">
+                                <Link className="" to="/music">
+                                    Music
+                                </Link>
+                            </li>
+                            <li className="pt-2">
+                                <Link className="" to="/sports">
+                                    Sports
+                                </Link>
+                            </li>
+                            <li className="pt-2">
+                                <Link className="" to="/art">
+                                    Art
+                                </Link>
+                            </li>
+                            <li className="pt-2">
+                                <Link className="" to="/specials">
+                                    Specials
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="w-1/2">
+                        <ul>
+                            <li className="pt-2">
+                                <Link className="" to="/">
+                                    About
+                                </Link>
+                            </li>
+                            <li className="pt-2">
+                                <Link className="" to="/music">
+                                    Contact
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="w-1/2 flex justify-between mt-5 md:w-2/12">
+                    <Link to="/">
+                        <FontAwesomeIcon
+                            className="w-7 h-7 hover:text-customGreen"
+                            icon={faInstagram}
+                        />
+                    </Link>
+                    <Link to="/">
+                        <FontAwesomeIcon
+                            className="w-7 h-7 hover:text-customGreen"
+                            icon={faTwitter}
+                        />
+                    </Link>
+                    <Link to="/">
+                        <FontAwesomeIcon
+                            className="w-7 h-7 hover:text-customGreen"
+                            icon={faLinkedinIn}
+                        />
+                    </Link>
+                </div>
+            </section>
         </div>
     );
 };
