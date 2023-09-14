d3d5f2d98e6d90995fd352cf5b2190d9d8e70f15
Emiliano <emikinderk@gmail.com>
2023-09-14 19:44:04 +0200

    vercel-0: fix case part1

--- src/pages/Index.jsx
+++
@@ -1,8 +0,0 @@
-import { Home } from "./Home";
-import { Art } from "./Art";
-import { Music } from "./Music";
-import { Sports } from "./Sports";
-import { NotFound } from "./NotFound";
-import { Login } from "./Login";
-import { Event } from "./Event";
-export { Home, Login, Art, Music, Sports, NotFound, Event };
