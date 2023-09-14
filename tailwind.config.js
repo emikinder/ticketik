549f382305e5228f145f7c5ff06b0b188f00c539
Emiliano <emikinderk@gmail.com>
2023-09-14 19:17:01 +0200

    vercel-0: config to deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ const { nextui } = require("@nextui-org/react");
 export default {
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx}",
         "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
